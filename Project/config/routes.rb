b56eaaf0160b28391b72ea87b3a2f9556946c55d
christianuminga <cuminga@academic.rrc.ca>
2018-10-22 11:01:53 -0500

    added erd and nav links

--- Project/config/routes.rb
+++ Project/config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
     end
   end
 
+  get 'search', to: 'pages#search'
   get 'about', to: 'pages#about'
 
 
--- Project/db/schema.rb
+++ Project/db/schema.rb
@@ -18,13 +18,6 @@ ActiveRecord::Schema.define(version: 2018_10_08_050506) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "pages", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "requests", force: :cascade do |t|
     t.datetime "date"
     t.string "location"
